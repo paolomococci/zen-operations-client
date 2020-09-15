ad655f8b53ecaa528d1260a46fc2a950e0a9da9d
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-15 10:33:49 +0200

    Update ZenOperationsClientApplicationTests.kt

--- src/test/kotlin/local/example/zen/ZenOperationsClientApplicationTests.kt
+++ src/test/kotlin/local/example/zen/ZenOperationsClientApplicationTests.kt
@@ -18,18 +18,13 @@
 
 package local.example.zen
 
-import org.junit.Assert.assertTrue
-import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.junit4.SpringRunner
 
-@RunWith(SpringRunner::class)
 @SpringBootTest
-class ZenOperationsClientApplicationTests {
+class ApplicationTests {
 
 	@Test
-	fun `sample test`() {
-		assertTrue(true)
+	fun voidTest() {
 	}
 }
