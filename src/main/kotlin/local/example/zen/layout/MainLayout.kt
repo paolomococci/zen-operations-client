0bdff36293f86a1798433533832ea1fa58d75c97
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-17 10:19:38 +0200

    Update MainLayout.kt

--- src/main/kotlin/local/example/zen/layout/MainLayout.kt
+++ src/main/kotlin/local/example/zen/layout/MainLayout.kt
@@ -19,19 +19,61 @@
 package local.example.zen.layout
 
 import com.vaadin.flow.component.applayout.AppLayout
+import com.vaadin.flow.component.applayout.DrawerToggle
 import com.vaadin.flow.component.dependency.CssImport
+import com.vaadin.flow.component.html.*
 import com.vaadin.flow.component.page.Push
 import com.vaadin.flow.component.page.Viewport
 import com.vaadin.flow.router.AfterNavigationEvent
 import com.vaadin.flow.router.AfterNavigationObserver
+import com.vaadin.flow.router.RouterLink
 import com.vaadin.flow.server.PWA
+import local.example.zen.view.MainView
+import local.example.zen.view.ItemView
 
 @Push
 @CssImport(value = "style.css")
 @PWA(enableInstallPrompt = false, name = "zen-operations-admin", shortName = "operations-admin", startPath = "/main")
 @Viewport(value = "width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover")
 class MainLayout : AppLayout(), AfterNavigationObserver {
-    override fun afterNavigation(event: AfterNavigationEvent?) {
-        TODO("Not yet implemented")
+
+    private var title: H1? = null
+    private var mainView: RouterLink? = null
+    private var itemView: RouterLink? = null
+
+    private fun listLinks(): Array<RouterLink?> {
+        return arrayOf(
+                mainView,
+                itemView
+        )
+    }
+
+    override fun afterNavigation(afterNavigationEvent: AfterNavigationEvent?) {
+        for (routerLink in listLinks()) {
+            if (routerLink!!.highlightCondition.shouldHighlight(
+                            routerLink,
+                            afterNavigationEvent
+                    )) {
+                title!!.text = routerLink.text
+            }
+        }
+    }
+
+    companion object {
+        fun mainLayout(mainLayout: MainLayout) {
+            mainLayout.title = H1("reactive RESTful web service data accessing")
+            mainLayout.mainView = RouterLink("main view", MainView::class.java)
+            mainLayout.itemView = RouterLink("someone view", ItemView::class.java)
+            val orderedList = OrderedList(
+                    ListItem(mainLayout.mainView),
+                    ListItem(mainLayout.itemView)
+            )
+            val header = Header(DrawerToggle(), mainLayout.title)
+            val nav = Nav(orderedList)
+            mainLayout.addToNavbar(header)
+            mainLayout.addToDrawer(nav)
+            mainLayout.primarySection = Section.DRAWER
+            mainLayout.isDrawerOpened = true
+        }
     }
 }
