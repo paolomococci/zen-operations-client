7aaa55b1064f4eed3c03406ab07b0c757eae8235
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-18 10:27:19 +0200

    Update ItemRestfulRetrieverService.kt

--- src/main/kotlin/local/example/zen/service/ItemRestfulRetrieverService.kt
+++ src/main/kotlin/local/example/zen/service/ItemRestfulRetrieverService.kt
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * 	   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed following in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,8 +15,26 @@
  * limitations under the License.
  *
  */
+package local.example.zen.service
 
-package local.example.zen.service;
+import local.example.zen.model.Item
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.hateoas.CollectionModel
+import org.springframework.hateoas.MediaTypes
+import org.springframework.hateoas.client.Traverson
+import java.net.URI
+import java.util.*
 
 class ItemRestfulRetrieverService {
+
+    fun getListOfItems(uri: URI?): ArrayList<Item?> {
+        val traverson = Traverson(uri!!, MediaTypes.HAL_JSON)
+        val traversalBuilder = traverson.follow("items")
+        val parameterizedTypeReference: ParameterizedTypeReference<CollectionModel<Item>> =
+                ParameterizedTypeReference.forType(Item::class.java)
+        var collectionModelOfItems: CollectionModel<Item>?
+        collectionModelOfItems = traversalBuilder.toObject(parameterizedTypeReference)
+        val collectionOfItems = collectionModelOfItems!!.content
+        return ArrayList<Item?>(collectionOfItems)
+    }
 }
