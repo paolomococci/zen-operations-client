d4a47fecb86956a8c0a4eacb9288ea3e4cb73aa3
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-19 19:15:09 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	implementation("eu.vaadinonkotlin:vok-framework:0.8.2")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("org.springframework.boot:spring-boot-starter-test") {
 		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
