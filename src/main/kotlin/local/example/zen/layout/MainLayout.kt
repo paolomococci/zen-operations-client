0aef8136b6b5831cadbfb6a360ff68dcaf08fd4c
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-09-19 09:18:56 +0200

    Update MainLayout.kt

--- src/main/kotlin/local/example/zen/layout/MainLayout.kt
+++ src/main/kotlin/local/example/zen/layout/MainLayout.kt
@@ -18,52 +18,13 @@
 
 package local.example.zen.layout
 
-import com.vaadin.flow.component.applayout.AppLayout
-import com.vaadin.flow.component.applayout.DrawerToggle
 import com.vaadin.flow.component.dependency.CssImport
-import com.vaadin.flow.component.html.*
 import com.vaadin.flow.component.page.Push
 import com.vaadin.flow.component.page.Viewport
-import com.vaadin.flow.router.AfterNavigationEvent
-import com.vaadin.flow.router.AfterNavigationObserver
-import com.vaadin.flow.router.RouterLink
 import com.vaadin.flow.server.PWA
-import local.example.zen.view.MainView
 
 @Push
 @CssImport(value = "style.css")
 @PWA(enableInstallPrompt = false, name = "zen-operations-admin", shortName = "operations-admin", startPath = "/main")
 @Viewport(value = "width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes, viewport-fit=cover")
-class MainLayout : AppLayout(), AfterNavigationObserver {
-
-    private var title: H1? = H1()
-    private lateinit var mainView: RouterLink
-
-    fun MainLayout() {
-        title = H1("reactive RESTful web service data accessing")
-        mainView = RouterLink("main view", MainView::class.java)
-        val unorderedList = OrderedList(
-                ListItem(mainView)
-        )
-        val header = Header(DrawerToggle(), title)
-        val nav = Nav(unorderedList)
-        this.addToNavbar(header)
-        addToDrawer(nav)
-        this.primarySection = Section.DRAWER
-        this.isDrawerOpened = false
-    }
-
-    private fun listLinks(): Array<RouterLink?>? {
-        return arrayOf(
-                mainView
-        )
-    }
-
-    override fun afterNavigation(afterNavigationEvent: AfterNavigationEvent?) {
-        for (link in this.listLinks()!!) {
-            if (link!!.highlightCondition.shouldHighlight(link, afterNavigationEvent)) {
-                title!!.text = link.text
-            }
-        }
-    }
-}
+class MainLayout
