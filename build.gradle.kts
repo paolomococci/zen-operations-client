8d9459b40e4c5d3326bf1bf9ab055d9a510ce6d5
paolomococci <37364757+paolomococci@users.noreply.github.com>
2020-12-01 00:06:00 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-	id("org.springframework.boot") version "2.3.5.RELEASE"
+	id("org.springframework.boot") version "2.4.0"
 	id("io.spring.dependency-management") version "1.0.10.RELEASE"
 	id("com.vaadin") version "0.8.0"
 	kotlin("jvm") version "1.4.10"
@@ -22,7 +22,7 @@ repositories {
 	mavenCentral()
 }
 
-extra["vaadinVersion"] = "14.4.2"
+extra["vaadinVersion"] = "14.4.3"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
