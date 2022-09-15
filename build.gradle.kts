0949422fe3b04e4da61935086cfb83edec1ec0d5
Paolo Mococci <37364757+paolomococci@users.noreply.github.com>
2022-09-15 09:04:21 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
 	id("org.springframework.boot") version "2.7.3"
 	id("io.spring.dependency-management") version "1.0.13.RELEASE"
-	id("com.vaadin") version "0.14.6.0"
+	id("com.vaadin") version "0.17.0.0"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
 }
