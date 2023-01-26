35bdd9d69c4ec9ada2f456fe45b19edb2988d4e1
paolomococci <paolomococci@gmail.com>
2023-01-26 19:19:13 +0100

    updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
 	id("org.springframework.boot") version "2.7.8"
 	id("io.spring.dependency-management") version "1.1.0"
 	id("com.vaadin") version "0.17.0.1"
-	kotlin("jvm") version "1.7.21"
-	kotlin("plugin.spring") version "1.7.21"
+	kotlin("jvm") version "1.7.22"
+	kotlin("plugin.spring") version "1.7.22"
 }
 
 group = "local.example"
