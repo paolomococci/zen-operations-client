893b6b8a4febd7b20ca9c53b02ca642ac39ca030
paolomococci <paolomococci@gmail.com>
2023-03-14 05:26:00 +0100

    updated

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 	mavenCentral()
 }
 
-extra["vaadinVersion"] = "14.9.6"
+extra["vaadinVersion"] = "14.9.7"
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-hateoas")
